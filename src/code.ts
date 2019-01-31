4c5ccf29c5b80318949336f0b0900cbdf5c4531e
Colin Walker <colinwalker270@gmail.com>
2019-01-31 07:39:41 -0800

    Code evaluation.

---
+++ src/code.ts
@@ -0,0 +1,12 @@
+export interface Code {
+  move: (req: any) => { move: string };
+  start: (req: any) => { color: string };
+}
+
+/**
+ * Code evaluates code and turns it into two functions that can be called.
+ */
+export function evaluate(code: string): Code {
+  const callable = new Function(`${code}; return { move: move, start: start }`)()
+  return callable;
+}
