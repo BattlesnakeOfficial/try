17828074319c42b0799e47d0a3e0464868286b61
Colin Walker <colinwalker270@gmail.com>
2019-01-31 07:39:56 -0800

    Add the app.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Grid, Button, Message } from "semantic-ui-react";
+import Editor from "./Editor";
+import Board from "./Board";
+import * as engine from "./engine";
+import * as code from "./code";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import { async } from "q";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+interface AppState {
+  code: string;
+  frame?: engine.Frame;
+  error?: Error;
+}
+
+class App extends React.Component<{}, AppState> {
+  state: AppState = {
+    code: ""
+  };
+
+  componentDidMount() {
+    this.setState({
+      code: localStorage.getItem('code') || '',
+    })
+  }
+
+  handleCodeChange = (code: string) => {
+    localStorage.setItem('code', code);
+    this.setState({ code });
+  };
+
+  handleStart = () => {
+    setTimeout(async () => {
+      try {
+        await engine.run(code.evaluate(this.state.code), frame => {
+          this.setState({ frame, error: undefined });
+        });
+      } catch (error) {
+        this.setState({ error });
+      }
+    }, 0);
+  };
+
+  render() {
+    const { frame, error, code } = this.state;
+    return (
+      <Grid style={{ height: "100%" }} divided="vertically">
+        <Grid.Row columns={3}>
+          <Grid.Column>
+            <p>Left</p>
+          </Grid.Column>
+          <Grid.Column style={{ height: "100%" }}>
+            <Editor value={code} onChange={this.handleCodeChange} />
+          </Grid.Column>
+          <Grid.Column>
+            {frame && (
+              <Board
+                food={frame.food}
+                columns={frame.game.width}
+                rows={frame.game.height}
+                snakes={[frame.snake]}
+              />
+            )}
+            {error && <Message color="red" content={error.message} />}
+            <Button onClick={this.handleStart}>Start</Button>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
